fb5f8b6bb4ae64b86c247afdcadd3dc19094eadd
aim_akonya <aim.akonya8@gmail.com>
2019-11-01 10:39:16 +0300

    setting up the server

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
+const http = require("http");
+const app = require('./app');
 
-console.log(8);
+const normalizePort = (val)=> {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)){
+        return val ;
+    }
+    if(port > 0){
+        return port ;
+    }
+    return false ;
+};
+
+const port =normalizePort(process.env.port || '8080');
+app.set('port', port);
+
+const errorHandler = error =>{
+    if(error.syscall !== 'listen'){
+        throw error; 
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port '+ port;
+
+    switch(error.code){
+        case 'EACCES':
+            console.error(bind + 'requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + 'is already in use.');
+            process.exit(1);
+            break
+        default:
+            throw error; 
+    }
+};
+
+const server = http.createServer(app);
+server.on('error', errorHandler);
+server.on('listen', ()=>{
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+})
+
+server.listen(port);
