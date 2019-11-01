594e9bdb0b752e7a50fd6ce3781ca3038f1c6be9
aim_akonya <aim.akonya8@gmail.com>
2019-11-01 16:17:54 +0300

    edited modules

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
-
+const headers = require('./middleware/headers');
 const app = express();
 
+
+//set cors headers
+app.use(headers);
+
+//initialise bodyparser
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 
 module.exports = app;
--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 
 module.exports = {
-    secret: " b'b\xe7d@P\x93(\xf3Z\x8e\xa89\x14\r)\xba\xcc\x08\xd9<' "
+    secret: " b'b\xe7d@P\x93(\xf3Z\x8e\xa89\x14\r)\xba\xcc\x08\xd9<' ",
+
+    db_username:'aim',
+    db_password:'swahili1010',
+    db_host: 'localhost',
+    db: 'teamwork',
+    db_port:'5432'
 }
