c808929801d90ec3a856efc77ed1b6a78f6b43df
aim_akonya <aim.akonya8@gmail.com>
2019-11-04 18:52:09 +0300

    d configurations

--- config.js
+++ config.js
@@ -2,9 +2,9 @@
 module.exports = {
     secret: " b'b\xe7d@P\x93(\xf3Z\x8e\xa89\x14\r)\xba\xcc\x08\xd9<' ",
 
-    db_username:'aim',
-    db_password:'swahili1010',
-    db_host: 'localhost',
-    db: 'teamwork',
+    db_username:'ndbyyxyaggaxsm',
+    db_password:'ab0e19f897ab3632de866e415570882b32430d0830e251c925b19592d8f04aa7',
+    db_host: 'ec2-174-129-253-162.compute-1.amazonaws.com',
+    db: 'dfe5q7mldhqmlr',
     db_port:'5432'
 }
